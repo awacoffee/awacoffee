4ef6a18db7a4fe4e8f7ebfb85c66f3e8b0e6536a
ozaki-koyo <ko3923tokutoku@gmail.com>
2022-02-25 16:15:27 +0900

    header.phpとfooter.phpを送ります。
    尾崎より

---
+++ 尾崎/footer.php
@@ -0,0 +1,64 @@
+<footer>
+    <div id="footer_wrap" class="wrap">
+        <div class="inner">
+            <!-- ▼フッター▼ -->
+            <div class="footer_flex">
+                <div class="footer_logo">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo_aoch.png" alt="Awa Our Coffee Hourのロゴ" />
+                </div>
+                <nav class="footer_nav">
+                    <ul class="footer_nav_list">
+                        <li class="footer_nav_item">
+                            <a href="">お店で飲みたい</a>
+                        </li>
+                        <li class="footer_nav_item">
+                            <a href="">豆を買いたい</a>
+                        </li>
+                        <li class="footer_nav_item">
+                            <a href="">条件検索</a>
+                        </li>
+                        <li class="footer_nav_item">
+                            <a href="">コラム</a>
+                        </li>
+                        <li class="footer_nav_item">
+                            <a href="">マイページ</a>
+                        </li>
+                    </ul>
+                    <ul class="footer_nav_list">
+                        <li class="footer_nav_item">
+                            <a href="">お問い合わせ</a>
+                        </li>
+                        <li class="footer_nav_item">
+                            <a href="">このサイトについて</a>
+                        </li>
+                        <li class="footer_nav_item">
+                            <a href="">プライバシーポリシー</a>
+                        </li>
+                        <li class="footer_nav_item">
+                            <a href=""><i class="fa-brands fa-instagram fa-2x"></i></a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            <!-- footer_flex -->
+
+            <p class="footer_wrap_copyright">
+                <small>Copyright &copy; 2022 動 All Rights
+                    Reserved.</small>
+            </p>
+        </div>
+        <!-- viewport -->
+    </div>
+    <!-- footer_wrap -->
+</footer>
+
+<!-- ▼Javascript▼ -->
+<!-- <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script> -->
+<!-- <script src="<?php echo get_template_directory_uri(); ?>/assets/js/front.js"></script> -->
+<!-- <script src="<?php echo get_template_directory_uri(); ?>/assets/js/store.js"></script> -->
+<!-- <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCHAjeGqygNhVIP1Jy4W45BdHirl7OWi8s"></script> -->
+<!-- <script src="<?php echo get_template_directory_uri(); ?>/assets/js/googlemap.js"></script> -->
+<?php wp_footer(); ?>
+</body>
+
+</html>
