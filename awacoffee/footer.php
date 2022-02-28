b21aaee4dec0e40bfe189f27b1e1b72c953b2c49
ozaki-koyo <ko3923tokutoku@gmail.com>
2022-02-28 16:11:04 +0900

    現時点のファイルを一式更新しました。
    尾崎より

--- awacoffee/footer.php
+++ awacoffee/footer.php
@@ -1,45 +1,50 @@
 <footer>
-    <div class="wrap">
+    <div id="footer_wrap" class="wrap">
         <div class="inner">
-            <!-- ▼フッターのナビリスト:仮!!▼ -->
-            <nav class="footer_nav">
-                <ul class="footer_nav_list">
-                    <li class="footer_nav_item">
-                        ※カンプ待ち
-                        <a href="">店で飲みたい</a>
-                    </li>
-                    <li class="footer_nav_item">
-                        <a href="">豆を買いたい</a>
-                    </li>
-                    <li class="footer_nav_item">
-                        <a href="">条件検索</a>
-                    </li>
-                    <li class="footer_nav_item">
-                        <a href="">コラム</a>
-                    </li>
-                    <li class="footer_nav_item">
-                        <a href="">コーヒー入門知識</a>
-                    </li>
-                    <li class="footer_nav_item">
-                        <a href="">マイページ</a>
-                    </li>
-                    <li class="footer_nav_item">
-                        <a href="">お問い合わせ</a>
-                    </li>
-                    <li class="footer_nav_item">
-                        <a href="">このサイトについて</a>
-                    </li>
-                    <li class="footer_nav_item">
-                        <a href="">プライバシーポリシー</a>
-                    </li>
-                </ul>
-            </nav>
-
-            <div class="footer_logo">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo_aoch.png" alt="" />
+            <!-- ▼フッター▼ -->
+            <div class="footer_flex">
+                <div class="footer_logo">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo_aoch.png" alt="Awa Our Coffee Hourのロゴ" />
+                </div>
+                <nav class="footer_nav">
+                    <ul class="footer_nav_list">
+                        <li class="footer_nav_item">
+                            <a href="">お店で飲みたい</a>
+                        </li>
+                        <li class="footer_nav_item">
+                            <a href="">豆を買いたい</a>
+                        </li>
+                        <li class="footer_nav_item">
+                            <a href="">条件検索</a>
+                        </li>
+                        <li class="footer_nav_item">
+                            <a href="">コラム</a>
+                        </li>
+                        <li class="footer_nav_item">
+                            <a href="">マイページ</a>
+                        </li>
+                    </ul>
+                    <ul class="footer_nav_list">
+                        <li class="footer_nav_item">
+                            <a href="">お問い合わせ</a>
+                        </li>
+                        <li class="footer_nav_item">
+                            <a href="">このサイトについて</a>
+                        </li>
+                        <li class="footer_nav_item">
+                            <a href="">プライバシーポリシー</a>
+                        </li>
+                        <li class="footer_nav_item">
+                            <a href=""><i class="fa-brands fa-instagram fa-2x"></i></a>
+                        </li>
+                    </ul>
+                </nav>
             </div>
+            <!-- footer_flex -->
+
             <p class="footer_wrap_copyright">
-                <small>Copyright &copy; 2022 動 All Rights Reserved.</small>
+                <small>Copyright &copy; 2022 動 All Rights
+                    Reserved.</small>
             </p>
         </div>
         <!-- viewport -->
@@ -47,35 +52,12 @@
     <!-- footer_wrap -->
 </footer>
 
-<!-- コンポーネント -->
-<!-- imgで -->
-<!-- <article class="column_wrap">
-        <div class="column_inner">
-            <div class="img_wrap">
-                <a href="#!">
-                    <img src="./img/column_sample.jpeg" alt="thumbnail" />
-                </a>
-            </div>
-            <div class="column_meta">
-                <time class="column_date" datetime="the_time"
-                    >2022年03月14日</time
-                >
-                <h3>とよとみ珈琲</h3>
-                <div class="column_desc">
-                    <p>自家焙煎にこだわった至福の一杯</p>
-                </div>
-                <div>
-                    <ul class="categories">
-                        <li>コラムカテゴリ</li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </article> -->
-
 <!-- ▼Javascript▼ -->
 <!-- <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script> -->
 <!-- <script src="<?php echo get_template_directory_uri(); ?>/assets/js/front.js"></script> -->
+<!-- <script src="<?php echo get_template_directory_uri(); ?>/assets/js/store.js"></script> -->
+<!-- <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCHAjeGqygNhVIP1Jy4W45BdHirl7OWi8s"></script> -->
+<!-- <script src="<?php echo get_template_directory_uri(); ?>/assets/js/googlemap.js"></script> -->
 <?php wp_footer(); ?>
 </body>
 
