494f1d76c3dfbb4ad98fb0edc4165bbf30f736ce
ozaki-koyo <ko3923tokutoku@gmail.com>
2022-03-04 16:09:14 +0900

    ヘッダーとフッターの各リンクを繋げました。
    尾崎より

---
+++ 0304/footer.php
@@ -0,0 +1,58 @@
+<footer>
+    <div id="footer_wrap" class="wrap">
+        <div class="inner">
+            <!-- ▼フッター▼ -->
+            <div class="footer_flex">
+                <div class="footer_logo">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo_aoch.png" alt="Awa Our Coffee Hourのロゴ" />
+                </div>
+                <nav class="footer_nav">
+                    <ul class="footer_nav_list">
+                        <li class="footer_nav_item">
+                            <a href="https://awa-coffee.com/purpose/drink/">お店で飲みたい</a>
+                        </li>
+                        <li class="footer_nav_item">
+                            <a href="https://awa-coffee.com/purpose/beans/">豆を買いたい</a>
+                        </li>
+                        <li class="footer_nav_item">
+                            <a href="<?php echo get_permalink(243); ?>">条件検索</a>
+                        </li>
+                        <li class="footer_nav_item">
+                            <a href="https://awa-coffee.com/column/">コラム</a>
+                        </li>
+                        <li class="footer_nav_item">
+                            <a href="<?php echo get_permalink(245); ?>">マイページ</a>
+                        </li>
+                    </ul>
+                    <ul class="footer_nav_list">
+                        <li class="footer_nav_item">
+                            <a href="<?php echo get_permalink(493); ?>">お問い合わせ</a>
+                        </li>
+                        <li class="footer_nav_item">
+                            <a href="">このサイトについて</a>
+                        </li>
+                        <li class="footer_nav_item">
+                            <a href="">プライバシーポリシー</a>
+                        </li>
+                        <li class="footer_nav_item">
+                            <a href="https://www.instagram.com/awa_our_coffee_hour/"><i class="fa-brands fa-instagram fa-2x"></i></a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            <!-- footer_flex -->
+
+            <p class="footer_wrap_copyright">
+                <small>Copyright &copy; 2022 動 All Rights
+                    Reserved.</small>
+            </p>
+        </div>
+        <!-- viewport -->
+    </div>
+    <!-- footer_wrap -->
+</footer>
+
+<?php wp_footer(); ?>
+</body>
+
+</html>
