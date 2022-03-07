76f30aa741870b9c86e3c9fa9d3c16d2cd551f5a
nishino-motoharu <xiye2730@gmail.com>
2022-03-07 13:40:21 +0900

    テンプレートパーツのareaとshopのPC版とSP版。columnを追加しました。

---
+++ 0307/loop-column.php
@@ -0,0 +1,26 @@
+<article class="column_wrap">
+    <a href="<?php the_permalink(); ?>">
+        <div class="column_inner">
+            <figure class="column_img_wrap">
+                <?php if (has_post_thumbnail()) : ?>
+                    <?php the_post_thumbnail(); ?>
+                <?php else : ?>
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/noimage2.jpg" alt="">
+                <?php endif; ?>
+                <!-- <img src="<?php echo get_template_directory_uri(); ?>/assets/img/4-3img.jpg" alt="コラム記事のサムネイル画像" /> -->
+            </figure>
+            <div class="column_meta">
+                <div class="categories_wrap">
+                    <time class="column_date" datetime="<?php the_time('Y-m-d'); ?>"><?php the_time('Y.m.d'); ?></time>
+                    <ul class="categories">
+                        <?php echo get_the_term_list($post->ID, 'column_category'); ?>
+                    </ul>
+                </div>
+                <h3><?php the_title(); ?></h3>
+                <div class="column_text">
+                    <p><?php the_excerpt(); ?></p>
+                </div>
+            </div>
+        </div>
+    </a>
+</article>
---
+++ 0307/loop-shop-sp.php
@@ -0,0 +1,27 @@
+<?php if (have_posts()) : ?>
+    <?php while (have_posts()) : the_post(); ?>
+        <article class="stores_card">
+            <a href="<?php echo get_permalink(); ?>">
+                <figure class="stores_img">
+                    <?php if (has_post_thumbnail()) : ?>
+                        <?php the_post_thumbnail(); ?>
+                    <?php else : ?>
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/noimage2.jpg" alt="">
+                    <?php endif; ?>
+                </figure>
+
+                <div class="text_inner">
+                    <h3 class="stores_name">
+                        <?php the_title(); ?>
+                    </h3>
+                    <div class="stores_catch"></div>
+                    <div class="stores_tags">
+                        <p class="stores_tag">
+                            <?php the_field('catchphrase'); ?>
+                        </p>
+                    </div>
+                </div>
+            </a>
+        </article>
+    <?php endwhile; ?>
+<?php endif; ?>
